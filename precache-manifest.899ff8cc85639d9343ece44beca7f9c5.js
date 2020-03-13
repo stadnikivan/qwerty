c4caa3c4b8dc4d64c8178b76076eacc77b67338b
stadnikivan <stadnik11ivan@gmail.com>
2020-03-13 08:45:30 +0200

    Updates

---
+++ precache-manifest.899ff8cc85639d9343ece44beca7f9c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60cb2282fe6497ffb2408327908700ec",
+    "url": "/qwerty/index.html"
+  },
+  {
+    "revision": "2be145402a22a42bd3dc",
+    "url": "/qwerty/static/css/main.0e60c061.chunk.css"
+  },
+  {
+    "revision": "12a87b85c8366f4d7da0",
+    "url": "/qwerty/static/js/2.88b3ae30.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/qwerty/static/js/2.88b3ae30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2be145402a22a42bd3dc",
+    "url": "/qwerty/static/js/main.2603fc60.chunk.js"
+  },
+  {
+    "revision": "e89272152970972fd87b",
+    "url": "/qwerty/static/js/runtime-main.a39fabd6.js"
+  }
+]);
